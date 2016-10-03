005b5fcfa07d64f313de8756662ddf63d1dc9711
Вадим <lenina125a116@yandex.ru>
2016-10-03 10:51:16 +0700

    Signed-off-by: Вадим <lenina125a116@yandex.ru>

--- spam.py
+++ spam.py
@@ -5,26 +5,29 @@ f = open('spambase/spambase.data', 'r')
 strings = f.read().split('\n')[0:-1]
 f.close()
 shuffle(strings)
-train_rows = len(strings) // 2
+rows = len(strings)
 columns = len(strings[0].split(',')) - 1
+set = np.zeros((rows, columns))
+answers = np.zeros((rows))
+for row in range(rows):
+	set[row] = strings[row].split(',')[0:-1]
+	answers[row] = strings[row].split(',')[-1]
+feature_means = np.mean(set, axis=0) 
+set -= feature_means 
+feature_std = np.std(set, axis=0) 
+set /= feature_std
+train_rows = rows // 2
 train_set = np.zeros((train_rows, columns))
-train_answers = np.zeros((train_rows))
-test_rows = len(strings) - train_rows
-test_set = np.zeros((test_rows, columns))
-test_answers = np.zeros(test_rows)
-for row in range(test_rows - 1):
-	train_set[row] = strings[row].split(',')[0:-1]
-	train_answers[row] = strings[row].split(',')[-1]
-for row in range(test_rows, len(strings)):
-	test_set[row - train_rows] = strings[row].split(',')[0:-1]
-	test_answers[row - train_rows] = strings[row].split(',')[-1]
-feature_means = np.mean(train_set, axis=0) 
-train_set -= feature_means 
-feature_std = np.std(train_set, axis=0) 
-train_set /= feature_std
+train_answers = np.zeros((rows))
+test_set = np.zeros((rows - train_rows, columns))
+test_answers = np.zeros((rows - train_rows))
+train_set = set[1:train_rows]
+test_set = set[train_rows:rows]
+train_answers = answers[1:train_rows]
+test_answers = answers[train_rows:rows]
 new_classifier = LogisticRegression()
 new_classifier.fit(X = train_set, y = train_answers, max_iters = 1000)
-train_errors = np.sum(new_classifier.predict(train_set) != train_answers)
-print ('%f percent errors in the train set. (Using your own train set fot testing is cheating, but you probably dont care anyway)' % (train_errors * 100.0 / len(test_set)))
+#train_errors = np.sum(new_classifier.predict(train_set) != train_answers)
+#print ('%f percent errors in the train set. (Using your own train set fot testing is cheating, but you probably dont care anyway)' % (train_errors * 100.0 / len(test_set)))
 test_errors = np.sum(new_classifier.predict(test_set) != test_answers)
 print ('%f percent errors in the test set' % (test_errors * 100.0 / len(test_set)))
