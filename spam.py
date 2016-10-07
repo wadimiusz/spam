6c872ab71d6a62853c6d0b058a4690f17e07e3f4
Вадим <lenina125a116@yandex.ru>
2016-10-07 15:12:39 +0700

    bugfixes

--- spam.py
+++ spam.py
@@ -1,29 +1,30 @@
 from logreg import LogisticRegression
 from random import shuffle
 import numpy as np
+import scipy as sp
 f = open('spambase/spambase.data', 'r')
 strings = f.read().split('\n')[0:-1]
 f.close()
 shuffle(strings)
 rows = len(strings)
 columns = len(strings[0].split(',')) - 1
-set = np.zeros((rows, columns))
-answers = np.zeros((rows))
+set = sp.zeros((rows, columns))
+answers = sp.zeros((rows))
 for row in range(rows):
 	set[row] = strings[row].split(',')[0:-1]
 	answers[row] = strings[row].split(',')[-1]
-feature_means = np.mean(set, axis=0) 
+feature_means = sp.mean(set, axis=0) 
 set -= feature_means 
-feature_std = np.std(set, axis=0) 
+feature_std = sp.std(set, axis=0) 
 set /= feature_std
 train_rows = rows // 2
-train_set = np.zeros((train_rows, columns))
-train_answers = np.zeros((rows))
-test_set = np.zeros((rows - train_rows, columns))
-test_answers = np.zeros((rows - train_rows))
-train_set = set[1:train_rows]
+train_set = sp.zeros((train_rows, columns))
+train_answers = sp.zeros((rows))
+test_set = sp.zeros((rows - train_rows, columns))
+test_answers = sp.zeros((rows - train_rows))
+train_set = set[:train_rows]
 test_set = set[train_rows:rows]
-train_answers = answers[1:train_rows]
+train_answers = answers[:train_rows]
 test_answers = answers[train_rows:rows]
 new_classifier = LogisticRegression()
 new_classifier.fit(X = train_set, y = train_answers, max_iters = 1000)
@@ -31,9 +32,12 @@ new_classifier.fit(X = train_set, y = train_answers, max_iters = 1000)
 #print ('%f percent errors in the train set. (Using your own train set fot testing is cheating, but you probably dont care anyway)' % (train_errors * 100.0 / len(test_set)))
 test_errors = np.sum(new_classifier.predict(test_set) != test_answers)
 print ('%f percent errors in the test set' % (test_errors * 100.0 / len(test_set)))
-TP = np.sum(new_classifier.predict(test_set) * test_answers)
-TN = np.sum((1 - new_classifier.predict(test_set)) * (1 -test_answers))
-FP = np.sum(new_classifier.predict(test_set) * (1- test_answers))
-FN = np.sum((1 - new_classifier.predict(test_set)) * test_answers)
+TP = sp.sum(new_classifier.predict(test_set) * test_answers)
+TN = sp.sum((1 - new_classifier.predict(test_set)) * (1 - test_answers))
+FP = sp.sum(new_classifier.predict(test_set) * (1 - test_answers))
+FN = sp.sum((1 - new_classifier.predict(test_set)) * test_answers)
+Sensitivity = TP / (TP + FN)
+Specitivity = TN / (TN + FP)
 print ('%d true positives, %d true negatives' % (TP, TN))
 print ('%d false positives, %d false negatives' % (FP, FN))
+print ('Sensitivity: %f, Specitivity: %f' % (Sensitivity, Specitivity))
